d5c5f216bd39cef93c0a50d26a4da6ba3fc9e082
pmoon <pmoon@riis.com>
2015-06-17 10:41:32 -0400

    Initial Commit

---
+++ UnitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitTest
+//
+//  Created by Paul Moon on 6/17/15.
+//  Copyright (c) 2015 RIIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
